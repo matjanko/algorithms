6303772e38aa02af9e864d0298d6cefaab1afe3e
matjanko <mateusz.jankowski@prokonto.pl>
2019-09-06 14:48:47 +0200

    Create unit test for bubble sort

---
+++ src/main/java/org/matjanko/algorithms/sorting/bubblesort/BubbleSort.java
@@ -0,0 +1,9 @@
+package org.matjanko.algorithms.sorting.bubblesort;
+
+import java.util.List;
+
+public class BubbleSort {
+    public static List<Integer> sort(List<Integer> list) {
+        return null;
+    }
+}
---
+++ src/test/java/org/matjanko/algorithms/sorting/bubblesort/BubbleSortTest.java
@@ -0,0 +1,34 @@
+package org.matjanko.algorithms.sorting.bubblesort;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class BubbleSortTest {
+    @Test
+    public void sortMethodTest() {
+        // given
+        List<Integer> sortedNumbers;
+        List<Integer> unsortedNumbers = getUnsortedNumbers();
+        List<Integer> expectedSortedNumbers = getSortedNumbers();
+
+        // when
+        sortedNumbers = BubbleSort.sort(unsortedNumbers);
+
+        // then
+        Assert.assertEquals(sortedNumbers, expectedSortedNumbers);
+    }
+
+    private List<Integer> getUnsortedNumbers() {
+        return new ArrayList<Integer>(
+                Arrays.asList(5, 7, 14, 21, 3, 38, 45, 2, 1, 5, 2, 14, 11, 0, -2));
+    }
+
+    private List<Integer> getSortedNumbers() {
+        return new ArrayList<Integer>(
+                Arrays.asList(-2, 0, 1, 2, 2, 3, 5, 5, 7, 11, 14, 14, 21, 38, 45));
+    }
+}
