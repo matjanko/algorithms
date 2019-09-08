97589acbee0a3b665c4e70e24b883a69f63b7676
matjanko <mateusz.jankowski@prokonto.pl>
2019-09-08 17:52:56 +0200

    Create test for insert sort algorithm

---
+++ src/main/java/org/matjanko/algorithms/sorting/insertsort/InsertSort.java
@@ -0,0 +1,9 @@
+package org.matjanko.algorithms.sorting.insertsort;
+
+import java.util.List;
+
+public class InsertSort {
+    public static void sort(List<Integer> list) {
+
+    }
+}
---
+++ src/test/java/org/matjanko/algorithms/sorting/insertsort/InsertSortTest.java
@@ -0,0 +1,34 @@
+package org.matjanko.algorithms.sorting.insertsort;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class InsertSortTest {
+    @Test
+    public void sortMethodTest() {
+        // given
+        List<Integer> unsortedNumbers = getUnsortedNumbers();
+        List<Integer> expectedSortedNumbers = getSortedNumbers();
+
+        // when
+        InsertSort.sort(unsortedNumbers);
+        List<Integer> sortedNumbers = new ArrayList<>(unsortedNumbers);
+
+        // then
+        Assert.assertEquals(sortedNumbers, expectedSortedNumbers);
+    }
+
+    private List<Integer> getUnsortedNumbers() {
+        return new ArrayList<Integer>(
+                Arrays.asList(5, 7, 14, 21, 3, 38, 45, 2, 1, 5, 2, 14, 11, 0, -2));
+    }
+
+    private List<Integer> getSortedNumbers() {
+        return new ArrayList<Integer>(
+                Arrays.asList(-2, 0, 1, 2, 2, 3, 5, 5, 7, 11, 14, 14, 21, 38, 45));
+    }
+}
